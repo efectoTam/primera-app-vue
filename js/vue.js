c961611eaa83ea3f40e98a3b82b36da4cdad5d0b
Tamara Luque <tam.luque@gmail.com>
2019-04-24 16:57:11 -0400

    life cycle on vue

--- js/vue.js
+++ js/vue.js
@@ -1,12 +1,35 @@
 var app =  new Vue({
   el: '.vue',
   data: {
-    mensaje: 'Aprende Vue.js fácilmente',
-    src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1200px-Vue.js_Logo_2.svg.png'
+    mensaje: 'Este es el mensaje'
+  },
+  beforeCreate: function() {
+    console.log('Llamando beforeCreate');
+  },
+  created: function() {
+    console.log('Llamando created');
+  },
+  beforeMount: function() {
+    console.log('Llamando beforeMount');
+  },
+  mounted: function() {
+    console.log('Llamando mounted');
+  },
+  beforeUpdate: function() {
+    console.log('Llamando beforeUpdate');
+  },
+  updated: function() {
+    console.log('Llamando updated');
+  },
+  beforeDestroy: function() {
+    console.log('Llamando beforeDestroy');
+  },
+  destroyed: function() {
+    console.log('Llamando destroyed');
   },
   methods: {
-    mostrarMensaje: function() {
-      return this.mensaje;
+    destruir: function () {
+      this.$destroy();
     }
   }
 })
