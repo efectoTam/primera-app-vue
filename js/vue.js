fff3b5567a2f6aa2593d819c809f41692ed470bb
Tamara Luque <tam.luque@gmail.com>
2019-04-24 15:49:52 -0400

    linking data

--- js/vue.js
+++ js/vue.js
@@ -1,7 +1,8 @@
 var app =  new Vue({
   el: '.vue',
   data: {
-    mensaje: 'Aprende Vue.js fácilmente'
+    mensaje: 'Aprende Vue.js fácilmente',
+    src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1200px-Vue.js_Logo_2.svg.png'
   },
   methods: {
     mostrarMensaje: function() {
