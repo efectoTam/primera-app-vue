5f3a0354d51f0123e1728eb19f73fe3f3fd73645
Tamara Luque <tam.luque@gmail.com>
2019-04-24 15:41:54 -0400

    interpolation

--- js/vue.js
+++ js/vue.js
@@ -2,5 +2,10 @@ var app =  new Vue({
   el: '.vue',
   data: {
     mensaje: 'Aprende Vue.js fácilmente'
+  },
+  methods: {
+    mostrarMensaje: function() {
+      return this.mensaje;
+    }
   }
 })
